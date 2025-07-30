9a5b0214d1709385962eab897db6b171eb7389c4
Justin <phoenicianorganics@gmail.com>
2025-07-30 16:32:47 -0700

    Initial Commit

---
+++ Movu/MovuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovuApp.swift
+//  Movu
+//
+//  Created by JG on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MovuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovuTests/MovuTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovuTests.swift
+//  MovuTests
+//
+//  Created by JG on 7/30/25.
+//
+
+import Testing
+@testable import Movu
+
+struct MovuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
