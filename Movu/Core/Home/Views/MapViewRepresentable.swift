bc8e6defe808e41a5bf54e97c6f92049bd3dc45d
Justin <phoenicianorganics@gmail.com>
2025-08-03 13:37:29 -0700

    Added Darkview App Icon Added mapview lat/long locator and coordinates Mapview now has annotation icons to show requested destination pin drop. Map now zooms out relative to user location

--- Movu/Core/Home/Views/MapViewRepresentable.swift
+++ Movu/Core/Home/Views/MapViewRepresentable.swift
@@ -11,6 +11,7 @@ struct MapViewRepresentable: UIViewRepresentable {
     
     let mapView = MKMapView()
     let locationManager = LocationManager()
+    @EnvironmentObject var locationViewModel: LocationSearchViewModel
     
     func makeUIView(context: Context) -> some UIView {
         mapView.delegate = context.coordinator
@@ -21,7 +22,10 @@ struct MapViewRepresentable: UIViewRepresentable {
         return mapView
     }
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        
+        if let coordinate = locationViewModel.selectedLocationCoordinate {
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+
+        }
     }
     
     func makeCoordinator() -> MapCoordinator {
@@ -31,17 +35,37 @@ struct MapViewRepresentable: UIViewRepresentable {
 
 extension MapViewRepresentable {
     class MapCoordinator: NSObject, MKMapViewDelegate {
+        
+        // Properties
+        
         let parent: MapViewRepresentable
+        
+        //Lifecycle
         
         init(parent: MapViewRepresentable) {
             self.parent = parent
             super.init()
         }
+        
+        // MKMapViewDelegate
+        
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             
             parent.mapView.setRegion(region, animated: true)
 
+        }
+        
+        // Helpers
+        
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
     }
 }
--- Movu/Core/LocationSearch/Views/LocationSearchView.swift
+++ Movu/Core/LocationSearch/Views/LocationSearchView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct LocationSearchView: View {
     @State private var startLocationText = ""
-    @State private var destinationLocationText = ""
-    @StateObject var viewModel = LocationSearchViewModel()
+    @Binding var showLocationSearchView: Bool
+    @EnvironmentObject var viewModel: LocationSearchViewModel
     
     var body: some View {
         VStack {
@@ -54,6 +54,10 @@ struct LocationSearchView: View {
                 VStack(alignment: .leading) {
                     ForEach(viewModel.results, id: \.self) { result in
                         LocationSearchResults(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                viewModel.selectLocation(result)
+                                showLocationSearchView.toggle()
+                            }
                     }
                 }
             }
@@ -62,6 +66,8 @@ struct LocationSearchView: View {
     }
 }
 
-#Preview {
-    LocationSearchView()
+struct LocationSearchView_Preview: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView(showLocationSearchView: .constant(false))
+    }
 }
